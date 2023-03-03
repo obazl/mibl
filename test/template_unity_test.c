e651a5fd6e0d29b45018f1d4aefd0deaa9f03272
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2023-03-03 16:43:46 -0600

    add unity test template

---
+++ test/template_unity_test.c
@@ -0,0 +1,25 @@
+#include "unity.h"
+#include "file_to_test.h"
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void test_function_should_doBlahAndBlah(void) {
+    //test stuff
+}
+
+void test_function_should_doAlsoDoBlah(void) {
+    //more test stuff
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_function_should_doBlahAndBlah);
+    RUN_TEST(test_function_should_doAlsoDoBlah);
+    return UNITY_END();
+}
