607396e7205620e219176f9921246e523c21e756
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2023-03-23 14:14:10 -0500

    remove obsolete test stuff

--- test/mytest2.scm
+++
@@ -1,7 +0,0 @@
-(define pkg
-  (let* ((_   (load "dune.scm"))
-         (arg "dune/stanzas/rule/action/mixed/two")
-         (pkgs (mibl-load-project arg))
-         (pkg (hash-table-ref pkgs arg))
-         (normed-pkg (dune-pkg->mibl pkg)))
-    normed-pkg))
